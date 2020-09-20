e72710151a0837b620d8a60b9b772cbfd68abbc7
unknown <pamelarozolem12@live.com>
2020-09-20 10:44:30 -0300

    primeira entrega

---
+++ src/primeiraEntrega/ExibeNumeroMaiorInt.java
@@ -0,0 +1,19 @@
+package primeiraEntrega;
+
+import javax.swing.JOptionPane;
+
+public class ExibeNumeroMaiorInt {
+        public static void main(String args[])
+    {
+       
+        int primeiroNumero = Integer.parseInt(JOptionPane.showInputDialog("Digite seu primeiro número"));
+        int segundoNumero = Integer.parseInt(JOptionPane.showInputDialog("Digite seu segundo número"));
+        
+        if(primeiroNumero > segundoNumero){
+            JOptionPane.showMessageDialog(null,"O maior número entre os dois é: " + primeiroNumero);
+        } else {
+             JOptionPane.showMessageDialog(null,"O maior número entre os dois é: " + segundoNumero);
+        }
+            
+    }
+}
